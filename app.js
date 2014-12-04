6eeaf61e8c409809766c4fa4437cbac10cd2e5ba
Marshal <marshal.wu@qq.com>
2014-12-04 11:58:16 +0800

    add code logging http

--- app.js
+++ app.js
@@ -16,7 +16,16 @@ app.set('view engine', 'hbs');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(__dirname + '/public/favicon.ico'));
-app.use(logger('dev'));
+if (app.get('env') === 'development') {
+    app.use(logger('dev'));
+}
+
+if (app.get('env') === 'production') {
+    var fs = require('fs')
+    var accessLogStream = fs.createWriteStream(__dirname + '/access.log', {flags: 'a'})
+    app.use(logger('combined', {stream: accessLogStream}))
+}
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
